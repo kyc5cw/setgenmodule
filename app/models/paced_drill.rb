f564c77151c804b5c843fbc439564a269405c28f
Prelang Builder <builder@prelang.com>
2015-07-08 19:10:30 +0000

    Generated model PacedDrill with migration

---
+++ app/models/paced_drill.rb
@@ -0,0 +1,4 @@
+class PacedDrill < ActiveRecord::Base
+  belongs_to :practice
+  belongs_to :drill
+end
---
+++ db/migrate/20150708191030_create_paced_drills.rb
@@ -0,0 +1,14 @@
+class CreatePacedDrills < ActiveRecord::Migration
+  def change
+    create_table :paced_drills do |t|
+      t.references :practice, index: true
+      t.references :drill, index: true
+      t.string :pace
+      t.string :pacing
+      t.decimal :order
+      t.string :group
+
+      t.timestamps
+    end
+  end
+end
